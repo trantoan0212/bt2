31cca1d2d60215a6bf0639cd28f65730aad60013
trantoan0212 <79898805+trantoan0212@users.noreply.github.com>
2021-05-28 14:54:00 +0700

    Add files via upload

---
+++ cau13.py
@@ -0,0 +1,25 @@
+import math
+ 
+def giai(a, b, c):
+    if (a == 0):
+        if (b == 0):
+            print ("Phương trình vô nghiệm!");
+        else:
+            print ("Phương trình có một nghiệm: x = ", + (-c / b));
+        return;
+    delta = b * b - 4 * a * c;
+    if (delta > 0):
+        x1 = (float)((-b + math.sqrt(delta)) / (2 * a));
+        x2 = (float)((-b - math.sqrt(delta)) / (2 * a));
+        print ("Phương trình có 2 nghiệm là: x1=", x1,"và x2=", x2);
+    elif (delta == 0):
+        x1 = (-b/(2*a));
+        print("Phương trình có nghiệm kép: x1 = x2 = ", x1);
+    else:
+        print("Phương trình vô nghiệm!");
+        
+a = float(input("a = "));
+b = float(input("b = "));
+c = float(input("c = "));
+
+giai(a, b, c)
